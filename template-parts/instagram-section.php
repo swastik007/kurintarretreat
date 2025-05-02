d7aea8361d2be1bcf10583e033b0f9cd38823ce4
Ashish Khadka <aasheeshkhadka@gmail.com>
2025-05-02 17:36:52 +0545

    insta

--- template-parts/instagram-section.php
+++ template-parts/instagram-section.php
@@ -1,4 +1,13 @@
 <!-- instagram_area_start -->
+<div class="row">
+    <div class="col-md-6 mx-auto text-center mb-5 section-heading">
+    <span>Stories</span>
+    <h3 class="title-heading">Instagram Posts</h3>
+        <p class="mt-3 text-muted">
+            Socialize
+        </p>
+    </div>
+</div>
 <div class="instragram_area">
     <?php
     $images = ['insta1.jpg', 'insta2.jpg', 'insta3.jpg', 'insta4.jpg', 'insta5.jpg', 'insta6.jpg'];
