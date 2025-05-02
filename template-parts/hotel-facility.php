260a96cd06e0bd4974bc72556939cb8e59725aa4
dineshkhadka9863 <dkhadka1000@gmail.com>
2025-05-02 16:05:45 +0545

    dinesh hotel facility added

---
+++ template-parts/hotel-facility.php
@@ -0,0 +1,185 @@
+<div id="hotel-facilities" style="height: 1187px;">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="section-title text-center">
+						<h2>Hotel Facilities</h2>
+					</div>
+				</div>
+			</div>
+
+			<div id="tabs">
+				<nav class="tabs-nav">
+					<a href="#" class="" data-tab="tab1">
+						<i class="flaticon-restaurant icon"></i>
+						<span>Restaurant</span>
+					</a>
+					<a href="#" data-tab="tab2" class="">
+						<i class="flaticon-cup icon"></i>
+						<span>Bar</span>
+					</a>
+					<a href="#" data-tab="tab3" class="active">
+					
+						<i class="flaticon-car icon"></i>
+						<span>Pick-up</span>
+					</a>
+					<a href="#" data-tab="tab4">
+						
+						<i class="flaticon-swimming icon"></i>
+						<span>Swimming Pool</span>
+					</a>
+					<a href="#" data-tab="tab5">
+						
+						<i class="flaticon-massage icon"></i>
+						<span>Spa</span>
+					</a>
+					<a href="#" data-tab="tab6">
+						
+						<i class="flaticon-bicycle icon"></i>
+						<span>Gym</span>
+					</a>
+				</nav>
+				<div class="tab-content-container">
+					<div class="tab-content" data-tab-content="tab1">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-6">
+									<img src="<?php echo get_template_directory_uri(); ?>/assets/images/bar1.jpg" class="img-responsive" alt="Image">
+								</div>
+								<div class="col-md-6">
+									<span class="super-heading-sm">World Class</span>
+									<h3 class="heading">Restaurant</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias officia perferendis modi impedit, rem quasi veritatis. Consectetur obcaecati incidunt, quae rerum, accusamus sapiente fuga vero at. Quia, labore, reprehenderit illum dolorem quae facilis reiciendis quas similique totam sequi ducimus temporibus ex nemo, omnis perferendis earum fugit impedit molestias animi vitae.</p>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam neque blanditiis eveniet nesciunt, beatae similique doloribus, ex impedit rem officiis placeat dignissimos molestias temporibus, in! Minima quod, consequatur neque aliquam.</p>
+									<p class="service-hour">
+										<span>Service Hours</span>
+										<strong>7:30 AM - 8:00 PM</strong>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tab-content" data-tab-content="tab2">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-6">
+									<img src="<?php echo get_template_directory_uri(); ?>/assets/images/bar1.jpg" class="img-responsive" alt="Image">
+								</div>
+								<div class="col-md-6">
+									<span class="super-heading-sm">World Class</span>
+									<h3 class="heading">Bars</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias officia perferendis modi impedit, rem quasi veritatis. Consectetur obcaecati incidunt, quae rerum, accusamus sapiente fuga vero at. Quia, labore, reprehenderit illum dolorem quae facilis reiciendis quas similique totam sequi ducimus temporibus ex nemo, omnis perferendis earum fugit impedit molestias animi vitae.</p>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam neque blanditiis eveniet nesciunt, beatae similique doloribus, ex impedit rem officiis placeat dignissimos molestias temporibus, in! Minima quod, consequatur neque aliquam.</p>
+									<p class="service-hour">
+										<span>Service Hours</span>
+										<strong>7:30 AM - 8:00 PM</strong>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tab-content active show" data-tab-content="tab3">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-6">
+									<img src="<?php echo get_template_directory_uri(); ?>/assets/images/bar1.jpg" class="img-responsive" alt="Image">
+								</div>
+								<div class="col-md-6">
+									<span class="super-heading-sm">World Class</span>
+									<h3 class="heading">Pick Up</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias officia perferendis modi impedit, rem quasi veritatis. Consectetur obcaecati incidunt, quae rerum, accusamus sapiente fuga vero at. Quia, labore, reprehenderit illum dolorem quae facilis reiciendis quas similique totam sequi ducimus temporibus ex nemo, omnis perferendis earum fugit impedit molestias animi vitae.</p>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam neque blanditiis eveniet nesciunt, beatae similique doloribus, ex impedit rem officiis placeat dignissimos molestias temporibus, in! Minima quod, consequatur neque aliquam.</p>
+									<p class="service-hour">
+										<span>Service Hours</span>
+										<strong>7:30 AM - 8:00 PM</strong>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tab-content" data-tab-content="tab4">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-6">
+									<img src="<?php echo get_template_directory_uri(); ?>/assets/images/bar1.jpg" class="img-responsive" alt="Image">
+								</div>
+								<div class="col-md-6">
+									<span class="super-heading-sm">World Class</span>
+									<h3 class="heading">Swimming Pool</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias officia perferendis modi impedit, rem quasi veritatis. Consectetur obcaecati incidunt, quae rerum, accusamus sapiente fuga vero at. Quia, labore, reprehenderit illum dolorem quae facilis reiciendis quas similique totam sequi ducimus temporibus ex nemo, omnis perferendis earum fugit impedit molestias animi vitae.</p>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam neque blanditiis eveniet nesciunt, beatae similique doloribus, ex impedit rem officiis placeat dignissimos molestias temporibus, in! Minima quod, consequatur neque aliquam.</p>
+									<p class="service-hour">
+										<span>Service Hours</span>
+										<strong>7:30 AM - 8:00 PM</strong>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tab-content" data-tab-content="tab5">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-6">
+									<img src="<?php echo get_template_directory_uri(); ?>/assets/images/bar1.jpg" class="img-responsive" alt="Image">
+								</div>
+								<div class="col-md-6">
+									<span class="super-heading-sm">World Class</span>
+									<h3 class="heading">Spa</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias officia perferendis modi impedit, rem quasi veritatis. Consectetur obcaecati incidunt, quae rerum, accusamus sapiente fuga vero at. Quia, labore, reprehenderit illum dolorem quae facilis reiciendis quas similique totam sequi ducimus temporibus ex nemo, omnis perferendis earum fugit impedit molestias animi vitae.</p>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam neque blanditiis eveniet nesciunt, beatae similique doloribus, ex impedit rem officiis placeat dignissimos molestias temporibus, in! Minima quod, consequatur neque aliquam.</p>
+									<p class="service-hour">
+										<span>Service Hours</span>
+										<strong>7:30 AM - 8:00 PM</strong>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tab-content" data-tab-content="tab6">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-6">
+									<img src="<?php echo get_template_directory_uri(); ?>/assets/images/bar1.jpg" class="img-responsive" alt="Image">
+								</div>
+								<div class="col-md-6">
+									<span class="super-heading-sm">World Class</span>
+									<h3 class="heading">Gym</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias officia perferendis modi impedit, rem quasi veritatis. Consectetur obcaecati incidunt, quae rerum, accusamus sapiente fuga vero at. Quia, labore, reprehenderit illum dolorem quae facilis reiciendis quas similique totam sequi ducimus temporibus ex nemo, omnis perferendis earum fugit impedit molestias animi vitae.</p>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam neque blanditiis eveniet nesciunt, beatae similique doloribus, ex impedit rem officiis placeat dignissimos molestias temporibus, in! Minima quod, consequatur neque aliquam.</p>
+									<p class="service-hour">
+										<span>Service Hours</span>
+										<strong>7:30 AM - 8:00 PM</strong>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll(".tabs-nav a");
+  const tabContents = document.querySelectorAll(".tab-content");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const tabId = this.getAttribute("data-tab");
+
+      navLinks.forEach((el) => el.classList.remove("active"));
+      this.classList.add("active");
+
+      tabContents.forEach((content) => {
+        if (content.getAttribute("data-tab-content") === tabId) {
+          content.classList.add("active", "show");
+        } else {
+          content.classList.remove("active", "show");
+        }
+      });
+    });
+  });
+});
+    </script>
