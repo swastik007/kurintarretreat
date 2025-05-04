4193c76209bad347653e5520a74f60de5cd38b7e
dineshkhadka9863 <dkhadka1000@gmail.com>
2025-05-04 14:07:42 +0545

    header changed and slider changed

--- header.php
+++ header.php
@@ -44,6 +44,17 @@
         ?>
       </nav>
 
+      <nav class="main-navigation-sticky">
+        <?php
+          wp_nav_menu(array(
+            'theme_location' => 'menu-1',
+            'menu_id'        => 'primary-menu',
+            'container'      => false,
+            'menu_class'     => 'menu-list-sticky',
+          ));
+        ?>
+      </nav>
+
       <!-- Booking Button -->
       <div class="book-btn">
       <a class="btn" href="javascript:void(0);" onclick="document.getElementById('booking-popup').style.display='block'">Book Now</a>
