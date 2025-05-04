cbc32d4857d1688007107b17f84b7fa5d07afcb6
dineshkhadka9863 <dkhadka1000@gmail.com>
2025-05-04 14:40:44 +0545

    change in blog page

--- template-parts/blogs-page.php
+++ template-parts/blogs-page.php
@@ -11,46 +11,41 @@
             <div class="row">
                 <div class="col-lg-4">
                     <div class="blog-item set-bg" data-setbg="<?php echo get_template_directory_uri(); ?>/assets/images/insta1.jpg">
+                        <span class="btn">Travel Trip</span>
                         <div class="bi-text">
-                            <span class="btn">Travel Trip</span>
                             <h3><a href="#">Tremblant In Canada</a></h3>
-                            <div class="b-time"><i class="icon_clock_alt"></i> 15th April, 2019</div>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4">
                     <div class="blog-item set-bg" data-setbg="<?php echo get_template_directory_uri(); ?>/assets/images/bar2.jpg">
+                        <span class="btn">Camping</span>
                         <div class="bi-text">
-                            <span class="btn">Camping</span>
                             <h3><a href="#">Choosing A Static Caravan</a></h3>
-                            <div class="b-time"><i class="icon_clock_alt"></i> 15th April, 2019</div>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4">
                     <div class="blog-item set-bg" data-setbg="<?php echo get_template_directory_uri(); ?>/assets/images/food1.jpg">
+                        <span class="btn">Event</span>
                         <div class="bi-text">
-                            <span class="btn">Event</span>
                             <h3><a href="#">Copper Canyon</a></h3>
-                            <div class="b-time"><i class="icon_clock_alt"></i> 21th April, 2019</div>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-8">
                     <div class="blog-item small-size set-bg" data-setbg="<?php echo get_template_directory_uri(); ?>/assets/images/event3.jpg" >
+                        <span class="btn">Event</span>
                         <div class="bi-text">
-                            <span class="btn">Event</span>
                             <h3><a href="#">Trip To Iqaluit In Nunavut A Canadian Arctic City</a></h3>
-                            <div class="b-time"><i class="icon_clock_alt"></i> 08th April, 2019</div>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4">
                     <div class="blog-item small-size set-bg" data-setbg="<?php echo get_template_directory_uri(); ?>/assets/images/family.jpg">
+                        <span class="btn">Travel</span>
                         <div class="bi-text">
-                            <span class="btn">Travel</span>
                             <h3><a href="#">Traveling To Barcelona</a></h3>
-                            <div class="b-time"><i class="icon_clock_alt"></i> 12th April, 2019</div>
                         </div>
                     </div>
                 </div>
