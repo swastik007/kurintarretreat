14caf84b458e5f3d2c28c4c0d0f236b9c4129f16
swastik <swastik@kodiary.com>
2025-05-06 10:34:54 +0545

    designs cleanup

--- template-parts/blogs-page.php
+++ template-parts/blogs-page.php
@@ -1,11 +1,12 @@
 <section class="blog-section">
     <div class="container">
         <div class="row">
-            <div class="col-lg-12">
-                <div class="text-center mb-5 icon-heading">
+            <div class="col-lg-8 mx-auto">
+                <div class="text-center mb-5 icon-heading section-heading">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-icon.png" />
-                    <span>Hotel News</span>
-                    <h3 class="title-heading">Our Blog &amp; Event</h3>
+                    <span>Blogs & Events</span>
+                    <h3 class="title-heading">Discover more with us</h3>
+                    <p>From travel tips and local highlights to wellness insights and resort experiences, our stories bring you closer to the charm of nature and the comfort of curated hospitality.</p>
                 </div>
             </div>
         </div>
@@ -14,7 +15,7 @@
                     <div class="blog-item set-bg" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/insta1.jpg')">
                         <span class="btn">Travel Trip</span>
                         <div class="bi-text">
-                            <h3><a href="#">Tremblant In Canada</a></h3>
+                            <h3><a href="#">Why Kurintar Retreat Is the Perfect Escape from City Life</a></h3>
                         </div>
                     </div>
                 </div>
@@ -22,7 +23,7 @@
                     <div class="blog-item set-bg" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/bar2.jpg')">
                         <span class="btn">Camping</span>
                         <div class="bi-text">
-                            <h3><a href="#">Choosing A Static Caravan</a></h3>
+                            <h3><a href="#">Luxury Meets Nature: Inside the Unique Charm of Kurintar</a></h3>
                         </div>
                     </div>
                 </div>
@@ -30,7 +31,7 @@
                     <div class="blog-item set-bg" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/food1.jpg')">
                         <span class="btn">Event</span>
                         <div class="bi-text">
-                            <h3><a href="#">Copper Canyon</a></h3>
+                            <h3><a href="#">A Day in the Life at Kurintar Retreat: What to Expect</a></h3>
                         </div>
                     </div>
                 </div>
@@ -38,7 +39,7 @@
                     <div class="blog-item small-size set-bg" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/event3.jpg" >
                         <span class="btn">Event</span>
                         <div class="bi-text">
-                            <h3><a href="#">Trip To Iqaluit In Nunavut A Canadian Arctic City</a></h3>
+                            <h3><a href="#">Must-Visit Places Around Kurintar</a></h3>
                         </div>
                     </div>
                 </div>
@@ -46,7 +47,7 @@
                     <div class="blog-item small-size set-bg" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/family.jpg')">
                         <span class="btn">Travel</span>
                         <div class="bi-text">
-                            <h3><a href="#">Traveling To Barcelona</a></h3>
+                            <h3><a href="#">Your Weekend Itinerary in Kurintar: What Not to Miss</a></h3>
                         </div>
                     </div>
                 </div>
